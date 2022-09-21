fefae4984354f6f166b68b1e587b8a32bd0f1ca0
Morsoth119 <morsoth119@gmail.com>
2022-09-21 17:15:02 +0200

    update

--- src/public/javascript/index.js
+++ src/public/javascript/index.js
@@ -1,12 +1,12 @@
 const meal_boxes = document.querySelectorAll(".meal-box");
 
-fetch("http://localhost:8000/dashboard/test")
+fetch("http://localhost:8000/dashboard/info")
     .then(response => response.json())
     .then(data => {
         data.forEach(elem => {
             meal_boxes[elem.meal - 1].innerHTML += `
             <div class="meal-item">
-                <h5>${elem.name} <button class="meal-delete"><i class="fa-solid fa-xmark"></i></button></h5>
+                <h5>${elem.name}<a class="meal-delete" href="http://localhost:8000/dashboard/delete/${elem.id}"><i class="fa-solid fa-xmark"></i></a></h5>
                 <span>${elem.kcal} kcal</span>
                 <span>${elem.carb}g</span>
                 <span>${elem.prot}g</span>
@@ -18,17 +18,3 @@ fetch("http://localhost:8000/dashboard/test")
             `;
         });
     });
-
-// Do smthing with the await
-
-const deleteBtns = document.querySelectorAll(".meal-delete");
-
-deleteBtns.forEach(elem => elem.addEventListener("input", getInfo()));
-
-async function getInfo() {
-    const data = await fetch("http://localhost:8000/dashboard/test")
-    .then(response => response.json())
-    .then(data => console.log(data));
-};
-
-console.log(deleteBtns);
--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -8,9 +8,25 @@ router.get("/", async (req, res) => {
     res.render("dashboard/dashboard", {meals});
 });
 
-router.get("/test", async (req, res) => {
+router.get("/info", async (req, res) => {
     const data = await pool.query("SELECT a.id, f.name, f.kcal, f.carb, f.prot, f.fats, a.meal, a.quantity FROM user_food_meal a, users u, foods f, meals m WHERE a.user = u.id AND a.food = f.id AND a.meal = m.id AND u.id = 1;");
     res.send(data);
 });
+
+router.get("/delete/:id", async (req, res) => {
+    const { id } = req.params;
+    await pool.query("DELETE FROM user_food_meal WHERE id = ?", [id]);
+    res.redirect("/");
+});
+
+router.get("/add", async (req, res) => {
+    res.render("/dashboard/add");
+});
+/*
+router.post("/add/:id", async (req, res) => {
+    const { id } = req.params;
+    await pool.query("INSERT INTO user_food_meal  VALUES (1, 1, 3, 1, 100);", [id]);
+    res.redirect("/");
+});*/
 
 module.exports = router;
