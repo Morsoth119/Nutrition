6c7c28825e436af8b8c4490505f22b1d6fae3569
Morsoth119 <morsoth119@gmail.com>
2022-09-25 14:26:12 +0200

    save

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const morgan = require("morgan");
 const hbs = require("express-handlebars");
 const path = require("path");
+const session = require("express-session");
+const passport = require("passport");
+
+require("./lib/passport");
 
 const app = express();
 app.set("port", process.env.PORT || 8000);
@@ -19,6 +23,8 @@ app.set("view engine", ".hbs"); // Para usar  hbs como motor de plantillas html
 app.use(morgan("dev"));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Routes
 app.use(require("./routes"));
